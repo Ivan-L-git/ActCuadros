ef693bc9a64501351f34b3f5356ef5860066c8cd
Ivan-L-git <324269440@alumno.enp.unam.mx>
2025-06-23 00:06:35 -0600

    Corrección de errores

--- cuadrados/Dynamics/js/generarCuadrados.js
+++ cuadrados/Dynamics/js/generarCuadrados.js
@@ -6,7 +6,7 @@ let detecta;
 let Cpar="par";
 let Cimpar="impar";
  
-for(let i=1; i<=10; i++) //establece el dígito de las decenas
+for(let i=1; i<=100; i++) //establece el dígito de las decenas
 {
     html += `<div class="fila">`;
     for(let j=1; j<=10; j++)
@@ -36,6 +36,7 @@ let todos_los_impares=document.getElementsByClassName("impar");
 let verifica_par=null;
 let verifica_impar=null;
 let verifica_primo=null;
+let ver_dos_casoEspecial=null;
 
 boton_par.addEventListener("click", ()=>{
     if(verifica_par==null) //verificación sobre si los pares están iluminados o no
@@ -81,35 +82,68 @@ boton_impar.addEventListener("click", ()=>{
 }
 );
 
-boton_primo.addEventListener("click", ()=>{
+boton_primo.addEventListener("click", ()=>
+{
     if(verifica_primo==null)
     {
         for (let ol of todos_los_impares)
         {
-            if(ol.textContent%5!==0 && ol.textContent%7!==0 && ol.textContent%2!==0 && ol.textContent%3!==0)
+            //importante el innerHTML, porque si no estoy comparando literalmente un "div" con un número
+            //lo mismo que si usará el textContent, porque te devuelve toda la etiqueta con su contenido
+            if(todos_los_pares[0].innerHTML==2) //esto es para que tome en cuenta el 2, porque es el punico número "par" que es primo
+            {
+                todos_los_pares[0].classList.add("iluminar_primos"); 
+            }
+            if(ol.textContent==3 ) //todo esto y similar, es para que tome en cuenta
+            //los numeros primos iniciales: 2, 3, 5 y 7
+            {
+               ol.classList.add("iluminar_primos"); 
+            }
+            if(ol.textContent==5)
+            {
+               ol.classList.add("iluminar_primos"); 
+            }
+            if(ol.textContent==7)
+            {
+               ol.classList.add("iluminar_primos"); 
+            }
+        
+            if(ol.textContent%5!==0 && ol.textContent%7!==0 && ol.textContent%3!==0 && ol.textContent>1)
             {
                         ol.classList.add("iluminar_primos");
                         verifica_primo=1;
-                        console.log("Es primo");
             }
         }
-        
     }
     else
     {
         for (let ul of todos_los_impares)
         {
-            if(ul.textContent%5!==0&&ul.textContent%7!==0&&ul.textContent%2!==0)
+            if(todos_los_pares[0].innerHTML==2) //esto es para que tome en cuenta el 2, porque es el punico número "par" que es primo
             {
-                    if(verifica_primo!==null)
-                    {
-                        ul.classList.remove("iluminar_primos");
-                        verifica_primo=null;
-                        console.log ("Hola Mundo")
-                    }
+                console.log(todos_los_pares[0]);
+                todos_los_pares[0].classList.remove("iluminar_primos"); 
+            }
+            if(ul.textContent==3)
+            {
+               ul.classList.remove("iluminar_primos"); 
+            }
+            if(ul.textContent==5)
+            {
+               ul.classList.remove("iluminar_primos"); 
+            }
+            if(ul.textContent==7)
+            {
+               ul.classList.remove("iluminar_primos"); 
+            }
+            if(ul.textContent%5!==0 && ul.textContent%7!==0 && ul.textContent%3!==0 && ul.textContent>1)
+            {
+                ul.classList.remove("iluminar_primos");
+                verifica_primo=null;
             }
         }
     }
+
 }
 );
 
--- cuadrados/Dynamics/js/pintarCuadrados.js
+++ cuadrados/Dynamics/js/pintarCuadrados.js
@@ -2,22 +2,4 @@
 //let cuadros_pares=document.getElementsByClassName("par");
 //let cuadros_impares=document.getElementsByClassName("impar");
 
-a=`
-let boton_par=document.getElementById("pares");
-let boton_impares=document.getElementById("impares");
-let boton_primos=document.getElementById("primos");
-
-boton_par.addEventListener("click", ()=>
-{
-    if(ilumina_pares==null) //verificación sobre si los pares están iluminados o no
-    {
-        ilumina_pares="ilumina_pares"; //si no están iluminados, los iluminará
-    }
-    else
-    {
-        ilumina_pares=null;
-    }
-}
-);
-`;
 
